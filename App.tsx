2061a4271dbcf3145ca758330dd195373e4b7f7b
BALAJI VASI <balajivasi@gmail.com>
2023-08-14 16:12:46 -0400

    added language from local

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {LanguageProvider} from './src/Context/LanguageProvider';
 import Home from './src/Screens/Home'; // Your main component
 import {SafeAreaView, StatusBar} from 'react-native';
+
 function Root() {
   return (
     <SafeAreaView>
