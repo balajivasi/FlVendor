2632c83787479e8c06646dacd58f38933f088cc5
BALAJI VASI <balajivasi@gmail.com>
2023-08-14 17:42:34 -0400

    added redux to app

--- App.tsx
+++ App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import {LanguageProvider} from './src/Context/LanguageProvider';
 import Home from './src/Screens/Home'; // Your main component
 import {SafeAreaView, StatusBar} from 'react-native';
+import {Provider} from 'react-redux';
+import {store} from './src/Store/Store';
 
 function Root() {
   return (
     <SafeAreaView>
       <StatusBar />
-      <LanguageProvider>
-        <Home />
-      </LanguageProvider>
+      <Provider store={store}>
+        <LanguageProvider>
+          <Home />
+        </LanguageProvider>
+      </Provider>
     </SafeAreaView>
   );
 }
--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
+
 function Home() {
     const { t } = useTranslation();
+    const message = useSelector(state => state.user.message);
     return (
         <View>
             <Text className="text-red-500">{t('welcome')}</Text>
+            <Text className="text-red-500">{message}</Text>
         </View>
     );
 }
